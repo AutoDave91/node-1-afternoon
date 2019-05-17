65e339be0294a78b9344ac8f6deda82bd1f41d2d
david <davidwnewman91@gmail.com>
2019-05-17 15:50:23 -0500

    Done

--- server/getProducts.js
+++ server/getProducts.js
@@ -1,7 +1,12 @@
 const products = require('../products.json');
 
 const getProducts = (req, res)=>{
-    res.status(200).send(products)
+    if(req.query.price){
+        const product = products.filter((product)=> product.price >= +req.query.price);
+        return res.status(200).send(product)
+    } else{
+        res.status(200).send(products)
+    }
 }
 
 module.exports = getProducts;
